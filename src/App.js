97d96873a4874a07899415908eb4a08a35019137
Khashayar Parhami <khparhami@gmail.com>
2020-07-27 21:35:10 +1000

    Adding validation and compare WIP needs to be separated

--- src/App.js
+++ src/App.js
@@ -1,43 +1,98 @@
 import React from "react";
-import { InputGroup, FormControl, Button } from "react-bootstrap";
-import TaxTable from "./TaxTable";
+import "bootstrap-css-only";
 import calculate from "./tax";
 import "./App.css";
+import TaxTable from "./TaxTable";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.incomeInputRef = React.createRef();
     this.taxTableRef = React.createRef();
+    this.financialYearRef = React.createRef();
     this.taxes = [];
   }
 
+  state = {
+    errorMessage: "",
+  };
+
+  handleValidation = () => {
+    const income_value = this.incomeInputRef.current.value;
+    const selected_financial_year = this.financialYearRef.current.value;
+
+    if (income_value === "") {
+      this.setState({ errorMessage: "Please enter your income" });
+      return false;
+    }
+
+    const row_already_added = this.taxes.filter(
+      (tax) =>
+        tax.income.toString() === income_value &&
+        tax.financial_year.toString() === selected_financial_year
+    );
+
+    if (row_already_added.length > 0) {
+      this.setState({ errorMessage: "Income already added" });
+      return false;
+    }
+    this.setState({ errorMessage: "" });
+    return true;
+  };
+
   handleClick = () => {
-    console.log("clicked");
+    if (!this.handleValidation()) {
+      return;
+    }
     let taxLine = calculate(this.incomeInputRef.current.value);
     taxLine.id = this.taxes.length + 1;
+    taxLine.financial_year = this.financialYearRef.current.value;
     this.taxes.push(taxLine);
     this.taxTableRef.current.refreshTaxes(this.taxes);
   };
 
   render() {
-    const taxes = [];
-    console.log("rendered");
     return (
       <div className="App">
-        <InputGroup className="mb-3">
-          <InputGroup.Prepend>
-            <InputGroup.Text>Income $</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl ref={this.incomeInputRef} type="text" />
-          <InputGroup.Append>
-            <InputGroup.Text>.00</InputGroup.Text>
-          </InputGroup.Append>
-        </InputGroup>{" "}
-        <Button onClick={this.handleClick} variant="secondary">
-          Add
-        </Button>{" "}
-        <TaxTable ref={this.taxTableRef} taxes={taxes} />
+        <div className="row">
+          <div className="col-lg-6">
+            <label htmlFor="income">Income</label>
+            <input
+              ref={this.incomeInputRef}
+              autoComplete="off"
+              className={"form-control"}
+              name="income"
+            />
+          </div>
+
+          <div className="col-lg-6">
+            <label htmlFor="financial_year">Financial year</label>
+            <select
+              name="financial_year"
+              className={"form-control"}
+              ref={this.financialYearRef}
+            >
+              <option>2019-2020</option>
+            </select>
+          </div>
+          <div
+            className={
+              this.state.errorMessage !== ""
+                ? "alert alert-danger"
+                : "alert alert-light"
+            }
+          >
+            {this.state.errorMessage}
+          </div>
+          <div className="col-lg-12  padd-top">
+            <button className="btn btn-success" onClick={this.handleClick}>
+              Add
+            </button>
+          </div>
+        </div>
+        <div>
+          <TaxTable ref={this.taxTableRef} taxes={this.taxes} />
+        </div>
       </div>
     );
   }
--- src/TaxTable.js
+++ src/TaxTable.js
@@ -5,6 +5,8 @@ class TaxTable extends React.Component {
   state = {
     selected: [0],
     taxes: [],
+    selected_taxes: [{}],
+    diff: [],
     columns: [
       {
         dataField: "id",
@@ -14,6 +16,11 @@ class TaxTable extends React.Component {
       {
         dataField: "income",
         text: "Income",
+        sort: true,
+      },
+      {
+        dataField: "financial_year",
+        text: "Financial year",
         sort: true,
       },
       {
@@ -51,12 +58,42 @@ class TaxTable extends React.Component {
 
   refreshTaxes = (taxes) => this.setState({ taxes: taxes });
 
+  compare = (current_row) => {
+    const selected_tax_1 = current_row;
+    const selected_tax_2 = this.state.selected_taxes[1];
+    const difference = {
+      id: 1,
+      income: selected_tax_1.income - selected_tax_2.income,
+      financial_year: "",
+      tax: Math.round(selected_tax_1.tax - selected_tax_2.tax),
+      medicare_levy: Math.round(
+        selected_tax_1.medicare_levy - selected_tax_2.medicare_levy
+      ),
+      low_income_tax_offset: Math.round(
+        selected_tax_1.low_income_tax_offset -
+          selected_tax_2.low_income_tax_offset
+      ),
+      low_mid_income_tax_offset: Math.round(
+        selected_tax_1.low_mid_income_tax_offset -
+          selected_tax_2.low_mid_income_tax_offset
+      ),
+      monthly_pay: selected_tax_1.monthly_pay - selected_tax_2.monthly_pay,
+      weekly_pay: selected_tax_1.weekly_pay - selected_tax_2.weekly_pay,
+    };
+
+    this.setState({ diff: difference });
+  };
+
   handleOnSelect = (row, isSelect) => {
     if (isSelect) {
       if (this.state.selected.length <= 2) {
         this.setState(() => ({
           selected: [...this.state.selected, row.id],
+          selected_taxes: [...this.state.selected_taxes, row],
         }));
+        if (this.state.selected.length === 2) {
+          this.compare(row);
+        }
       } else {
         this.setState(() => ({
           selected: this.state.selected,
@@ -84,6 +121,11 @@ class TaxTable extends React.Component {
           data={this.state.taxes}
           columns={this.state.columns}
           selectRow={selectRow}
+        />
+        <BootstrapTable
+          keyField="id"
+          data={this.state.diff}
+          columns={this.state.columns}
         />
       </div>
     );
